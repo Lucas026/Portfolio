e8b2c88e0fd4f4d033825224410d4d33536c74f8
Lucas026 <lucas02670@gmail.com>
2026-07-02 12:04:48 +0200

    creation bases pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ExperienceRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\SkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+     #[Route('/', name: 'app_home')]
+    public function index(
+        ProjectRepository $projectRepository,
+        SkillRepository $skillRepository,
+        ExperienceRepository $experienceRepository,
+    ): Response {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'featuredProjects' => $projectRepository->findBy(['featured' => true], ['position' => 'ASC'], 3),
+            'skills' => $skillRepository->findAll(),
+            'experiences' => $experienceRepository->findBy([], ['startDate' => 'DESC']),
         ]);
     }
 }
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProjectController extends AbstractController
+class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function index(): Response
+    #[Route('/projets', name: 'app_project_index')]
+    public function index(ProjectRepository $projectRepository): Response
     {
         return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+            'projects' => $projectRepository->findBy([], ['position' => 'ASC']),
+        ]);
+    }
+
+    #[Route('/projets/{slug}', name: 'app_project_show')]
+    public function show(string $slug, ProjectRepository $projectRepository): Response
+    {
+        $project = $projectRepository->findOneBy(['slug' => $slug]);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Projet introuvable.');
+        }
+
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
         ]);
     }
 }
