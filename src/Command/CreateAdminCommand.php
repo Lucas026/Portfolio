1126911b0f2d5f872e0f52ae53e39edffedb7c96
Lucas026 <lucas02670@gmail.com>
2026-07-02 11:59:23 +0200

    controller + crud + install tailwind

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-admin', description: 'Crée un compte administrateur')]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email de l\'admin')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $user = new User();
+        $user->setEmail($input->getArgument('email'));
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $input->getArgument('password'))
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('Compte admin créé : ' . $user->getEmail());
+
+        return Command::SUCCESS;
+    }
+}
